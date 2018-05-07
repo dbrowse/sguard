6afbc10429482446a70f782de71b2b9dbda9bc28
Diyan <dgeorgiev_job@yahoo.com>
2018-05-07 16:34:33 +0200

    Create rails app and add first status

---
+++ db/migrate/20180507142108_create_statuses.rb
@@ -0,0 +1,11 @@
+class CreateStatuses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :statuses do |t|
+      t.string :name
+      t.string :content
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
